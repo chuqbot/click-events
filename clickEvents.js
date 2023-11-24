9298345aa815e59019ca1a07075448d89cc0c5c7
Jeremy C <jchuq27@gmail.com>
2023-11-23 21:32:25 -0800

    commit

---
+++ clickEvents.js
@@ -0,0 +1,25 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+button.addEventListener('click' , responseToClick)
+
+function responseToClick() {
+    const h3Tag = document.createElement( 'h3');
+    h3Tag.textContent = "Foo";
+    body.appendChild(h3Tag);
+}
+
+const bbutton = document.querySelector("bbutton");
+bbutton.addEventListener('click' , response)
+function response() {
+    const h3Tag = document.createElement('h3');
+    h3Tag.textContent = "Bar";
+    body.appendChild(h3Tag);
+}
+
+const fbutton = document.querySelector("fbutton");
+fbutton.addEventListener('click', result)
+function result() {
+    const h2Tag = document.createElement('h2');
+    h2Tag.textContent = "Foobar";
+    body.appendChild(h2Tag);
+}
