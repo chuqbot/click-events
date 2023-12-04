831d9b2d48216b3f7e8bb7c51b10f03ee7d67557
Jeremy C <jchuq27@gmail.com>
2023-12-03 21:28:11 -0800

    lol

--- clickEvents.js
+++ clickEvents.js
@@ -2,8 +2,9 @@ const button = document.querySelector("button");
 const body = document.querySelector("body");
 button.addEventListener('click' , responseToClick)
 
+
 function responseToClick() {
-    const h3Tag = document.createElement( 'h3');
+    const h3Tag = document.createElement('h3');
     h3Tag.textContent = "Foo";
     body.appendChild(h3Tag);
 }
